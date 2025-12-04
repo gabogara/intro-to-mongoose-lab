010952d87f1d5e3ec0f59359e4ef106db7bf9e1a
gabogara <gabo1gara@gmail.com>
2025-12-04 14:29:02 -0800

    Add CLI menu with customer options

--- app.js
+++ app.js
@@ -6,21 +6,53 @@ const prompt = require("prompt-sync")();
 
 const Customer = require("./models/customer");
 
-const username = prompt("What is your name? ");
+const showMenu = async () => {
+  let running = true;
 
-console.log(`Your name is ${username}`);
+  while (running) {
+    console.log("\nWelcome to the CRM");
+    console.log("What would you like to do?");
+    console.log("1. Create a customer");
+    console.log("2. View all customers");
+    console.log("3. Update a customer");
+    console.log("4. Delete a customer");
+    console.log("5. Quit");
 
-const connect = async () => {
+    const choice = prompt("Number of action to run: ");
+
+    switch (choice) {
+      case "1":
+        console.log("opcion Crear");
+        break;
+      case "2":
+        console.log("opcion update");
+        break;
+      case "3":
+        console.log("opcion read");
+        break;
+      case "4":
+        console.log("opcion delete");
+        break;
+      case "5":
+        running = false;
+        console.log("Goodbye!");
+        break;
+      default:
+        console.log("Invalid choice, please try again.");
+    }
+  }
+};
+
+const main = async () => {
   // Connect to MongoDB using the MONGODB_URI specified in our .env file.
   await mongoose.connect(process.env.MONGODB_URI);
   console.log("Connected to MongoDB");
 
-  // Disconnect our app from MongoDB after our queries run.
+  await showMenu();
   await mongoose.disconnect();
   console.log("Disconnected from MongoDB");
-
   // Close our app, bringing us back to the command line.
   process.exit();
 };
 
-connect();
+main();
