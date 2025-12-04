9ba7bc691f2c0315610ed35bfd3804fe726fa2b6
gabogara <gabo1gara@gmail.com>
2025-12-03 23:43:49 -0800

    add the connect function to connect to mongoDB

--- app.js
+++ app.js
@@ -9,3 +9,18 @@ const Customer = require("./models/customer");
 const username = prompt("What is your name? ");
 
 console.log(`Your name is ${username}`);
+
+const connect = async () => {
+  // Connect to MongoDB using the MONGODB_URI specified in our .env file.
+  await mongoose.connect(process.env.MONGODB_URI);
+  console.log("Connected to MongoDB");
+
+  // Disconnect our app from MongoDB after our queries run.
+  await mongoose.disconnect();
+  console.log("Disconnected from MongoDB");
+
+  // Close our app, bringing us back to the command line.
+  process.exit();
+};
+
+connect();
